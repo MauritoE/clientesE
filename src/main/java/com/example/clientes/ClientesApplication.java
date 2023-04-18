ec3f539bdd88b5a110adcb60105c30cf21e75d2f
MauritoE <carlos.espitia@zabud.com.co>
2023-04-17 23:39:28 -0500

    cambio

---
+++ src/main/java/com/example/clientes/ClientesApplication.java
@@ -0,0 +1,37 @@
+package com.example.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+
+@SpringBootApplication
+public class ClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(
+				Arrays.asList("http://localhost:3000/", "http://localhost:4200"));
+						corsConfiguration.setAllowedHeaders(
+								Arrays.asList("Access-Control-Allow-Origin", "Content-Type",
+										"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+										"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setAllowedMethods(
+				Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
+}
+
